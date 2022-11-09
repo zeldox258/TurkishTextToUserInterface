35111769c69e6d34bbe680c730b670e9355f4e68
Omer <hsynmrgry@gmail.com>
2022-11-09 13:11:08 +0300

    initial begin

---
+++ src/turkish/text/to/code/TurkishTextToCode.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package turkish.text.to.code;
+
+/**
+ *
+ * @author omera
+ */
+public class TurkishTextToCode {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new mainScreen().setVisible(true);
+    }
+    
+}
