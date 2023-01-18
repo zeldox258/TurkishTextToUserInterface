88ce63defb7624ecbf87dc7a65ddefec72893b55
Omer <hsynmrgry@gmail.com>
2023-01-18 18:21:25 +0300

    minimal changes

--- src/turkish/text/to/code/mainScreen.java
+++ src/turkish/text/to/code/mainScreen.java
@@ -701,7 +701,7 @@ public class mainScreen extends javax.swing.JFrame {
                                 "</body>\n" +
                                 "<script src=\"https://cdn.jsdelivr.net/npm/file-saver@2.0.2/dist/FileSaver.min.js\"></script>"+
                                 "<script src=\"./main.js\"></script>"+
-                                "</html>");
+                                "</html>"); 
 
 
             printStream3.close();
